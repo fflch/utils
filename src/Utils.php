b13217883de66489b51c6987c1dda3973f458e42
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-06-09 10:49:52 -0300

    date convertion

--- src/Utils.php
+++ src/Utils.php
@@ -2,6 +2,9 @@
 
 namespace Uspdev;
 
+/**
+ * Utils Class
+ */
 class Utils
 {
     /* https://scripts.guru/partially-hide-email-address-using-php/ */
@@ -11,6 +14,18 @@ class Utils
         $name = implode(array_slice($em, 0, count($em)-1), '@');
         $len  = floor(strlen($name)/2);
         return substr($name,0, $len) . str_repeat('*', $len) . "@" . end($em);
+    }
+
+    /* Recebe YYYY-MM-DD e retorna DD/MM/YYYY */
+    public static function data_ptbr($data)
+    {
+        return implode('/',array_reverse(explode('-',$data)));
+    }
+
+    /* Recebe DD/MM/YYYY e devolve YYYY-MM-DD */
+    public static function data_en($data)
+    {
+       return implode('-',array_reverse(explode('/',$data)));
     }
 
 }
