59a52c9006e3f8933947f0670ffd9b4c514ffcc2
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-05-19 17:58:41 -0300

    data intervalo

--- src/Generic.php
+++ src/Generic.php
@@ -2,6 +2,8 @@
 
 namespace Uspdev\Utils;
 
+use Carbon\Carbon;
+
 /**
  * Utils Class
  */
@@ -44,6 +46,67 @@ class Generic
           }
       }
       return $result;
+    }
+
+    /**
+     * Calcula a diferença entre duas datas no formato d/m/Y.
+     * Exemplo de retorno:
+     * 2 ano(s), 1 mês(es) e 10 dia(s)
+     * 1 ano, 2 meses e 1 dia
+     * 1 ano e 10 meses
+     * 10 meses e 1 dia 
+     * 1 ano e 2 dias
+     */
+    public static function formata_periodo($inicio, $fim) {
+
+        $data_inicial = Carbon::createFromFormat("d/m/Y", $inicio);
+        $data_final = Carbon::createFromFormat("d/m/Y", $fim);
+
+        # Vamos somar 1 dia para arredondamento ?
+        #$diff = $data_inicial->diff($data_final->addDays(1));
+        $diff = $data_inicial->diff($data_final);
+
+        $text = '';
+
+        # Tratamento dos anos
+        if($diff->format('%y') > 1){
+            $text .= $diff->format('%y anos');
+
+        } else if($diff->format('%y') == 1){
+            $text .= $diff->format('%y ano');
+        }
+
+        # Divisor entre ano e mês: nada, virgula ou "e"
+        if(!empty($text)){
+            if($diff->format('%d') > 0 && $diff->format('%m') !=0 ){
+                $text .= ', ';
+            } else if($diff->format('%d') == 0 && $diff->format('%m') !=0){
+                $text .= ' e ';
+            }
+        }
+
+        # Tratamento dos meses
+        if($diff->format('%m') > 1){
+            $text .= $diff->format('%m meses');
+
+        } else if($diff->format('%m') == 1){
+            $text .= $diff->format('%m mês');
+        }
+
+        # Divisor entre mês/ano e dia
+        if(!empty($text) && $diff->format('%d') > 0){
+            $text .= ' e ';
+        }
+
+        # Tratamento dos dias
+        if($diff->format('%d') > 1){
+            $text .= $diff->format('%d dias');
+
+        } else if($diff->format('%d') == 1){
+            $text .= $diff->format('%d dia');
+        }
+
+        return $text;
     }
 
 }
--- tests/GenericTest.php
+++ tests/GenericTest.php
@@ -13,4 +13,15 @@ class GenericTest extends TestCase
     $to = 'maria.jo********@gmail.com';
     $this->assertEquals($to, Generic::partially_email($from));
   }
+
+  public function test_formata_periodo()
+  {
+    $this->assertEquals('1 dia', Generic::formata_periodo('01/01/2020','02/01/2020'));
+    $this->assertEquals('1 ano', Generic::formata_periodo('01/01/2020','01/01/2021'));
+    $this->assertEquals('1 ano, 1 mês e 2 dias', Generic::formata_periodo('01/01/2020','03/02/2021'));
+    $this->assertEquals('2 anos, 4 meses e 2 dias', Generic::formata_periodo('01/01/2020','03/05/2022'));
+    $this->assertEquals('4 meses e 1 dia', Generic::formata_periodo('01/01/2020','02/05/2020'));
+    $this->assertEquals('4 meses', Generic::formata_periodo('01/01/2020','01/05/2020'));
+    $this->assertEquals('1 ano e 3 meses', Generic::formata_periodo('01/01/2020','01/04/2021'));
+  }
 }
